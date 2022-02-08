40ddd225e39e55162d285bea11bd5d4e2d01cd9e
Ting Deng <dengting0122@gmail.com>
2022-02-08 11:53:25 -0800

    add role repositores and role view models

---
+++ ThriftBook_phase2/ViewModels/UserVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThriftBook_phase2.ViewModels
+{
+    public class UserVM
+    {
+        [Key]
+        public string Email { get; set; }
+    }
+}

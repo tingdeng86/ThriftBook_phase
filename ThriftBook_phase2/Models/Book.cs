990576d056ff24ddaaa8011e761b57d3f38f234e
SimonLeesk <93342725+SimonLeesk@users.noreply.github.com>
2022-02-08 15:18:12 -0800

    add entities

--- ThriftBook_phase2/Models/Book.cs
+++ ThriftBook_phase2/Models/Book.cs
@@ -25,5 +25,6 @@ namespace ThriftBook_phase2.Models
         public virtual Store StoreNameNavigation { get; set; }
         public virtual ICollection<BookRating> BookRatings { get; set; }
         public virtual ICollection<Invoice> Invoices { get; set; }
+        public virtual ICollection<BookInvoice> BookInvoices { get; set; }
     }
 }

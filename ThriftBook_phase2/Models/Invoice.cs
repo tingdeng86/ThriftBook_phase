48074ca8448ab2d9e724f83154dc5001404d6ecd
Adrian Sieroslawski <adrian.sieroslawski@gmail.com>
2022-02-08 11:56:56 -0800

    Added Buyer, Invoice, Store models

---
+++ ThriftBook_phase2/Models/Invoice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rolesDemoSSD.Models
+{
+    public class Invoice
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransactionId { get; set; }
+        public int? BuyerId { get; set; }
+        public double? TotalPrice { get; set; }
+        public DateTime? DateOfTransaction { get; set; }
+
+        public virtual Buyer Buyer { get; set; }
+        public virtual ICollection<BookInvoice> BookInvoices { get; set; }
+    }
+}
---
+++ ThriftBook_phase2/Models/Store.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rolesDemoSSD.Models
+{
+    public class Store
+    {
+        [Key]
+        public string StoreName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}

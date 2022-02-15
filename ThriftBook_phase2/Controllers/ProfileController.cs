6d416338daefb6331ba2fc583a963b3e566b8465
Dana Golubiev <48007635+DanaGolub@users.noreply.github.com>
2022-02-15 11:42:32 -0800

    Add Profile Controller and View (Details).

---
+++ ThriftBook_phase2/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThriftBook_phase2.Data;
+
+namespace ThriftBook_phase2.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<ProfileController> _logger;
+        private readonly ApplicationDbContext _context;
+        public ProfileController(ILogger<ProfileController> logger,
+                              ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Profile()
+        {
+            string userName = User.Identity.Name;
+
+            return View();
+        }
+    }
+}

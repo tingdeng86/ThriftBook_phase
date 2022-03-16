82f2d49dbd3ba0524261703fe41233898e2adaa2
Adrian Sieroslawski <adrian.sieroslawski@gmail.com>
2022-03-16 11:33:04 -0700

    Created functionality to download a CSV file for all invoices

--- ThriftBook_phase2/Controllers/BuyerListController.cs
+++ ThriftBook_phase2/Controllers/BuyerListController.cs
@@ -4,6 +4,7 @@ using rolesDemoSSD.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ThriftBook_phase2.Data;
 using ThriftBook_phase2.Repositories;
@@ -36,6 +37,20 @@ namespace ThriftBook_phase2.Controllers
             IQueryable<InvoiceDetailVM> iVM = iRepo.Get(transactionID);
             ViewBag.TotalAmount = iVM.Sum(x => (double)x.Price);
             return View(iVM);
+        }
+
+        public IActionResult ExportToCSV()
+        {
+            InvoiceRepo iRepo = new InvoiceRepo(_context);
+            IQueryable<InvoiceVM> iVM = iRepo.GetAll();
+            var builder = new StringBuilder();
+            builder.AppendLine("Buyer ID,Total Price,Transaction Date,First Name,Last Name,Phone Number,Email,Postal Code");
+            foreach (var item in iVM)
+            {
+                builder.AppendLine($"{item.BuyerId},{item.TotalPrice}, {item.DateOfTransaction}, {item.FirstName}, {item.LastName}, {item.PhoneNumber}, {item.Email}, {item.PostalCode}");
+
+            }
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "invoice.csv");
         }
     }
 }

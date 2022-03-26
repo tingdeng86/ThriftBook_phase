5e404eb84ecf3664752e858f116177ee8b292e9f
Ting Deng <dengting0122@gmail.com>
2022-03-25 22:16:11 -0700

    create a ordersHistoryController and ordersHistoryRepo for buyers getting the orders history

--- ThriftBook_phase2/Controllers/OrdersHistoryController.cs
+++ ThriftBook_phase2/Controllers/OrdersHistoryController.cs
@@ -1,16 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using rolesDemoSSD.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ThriftBook_phase2.Data;
+using ThriftBook_phase2.Repositories;
 
 namespace ThriftBook_phase2.Controllers
 {
     public class OrdersHistoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public OrdersHistoryController( ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            ViewData["Message"] = "";
+            string userEmail = User.Identity.Name;
+            ProfileRepo prRepo = new ProfileRepo(_context);
+            int buyerId = prRepo.GetLoggedInUser(userEmail).BuyerId;
+            OrdersHistoryRepo oRepo = new OrdersHistoryRepo(_context);
+            IQueryable<Invoice> iVM = oRepo.GetOrdersLists(buyerId);
+            if (iVM == null)
+            {
+                ViewData["Message"] = "No orders history.";
+            }
+            return View(iVM);
+            
+        }
+
+        public IActionResult OrderDetail(string paymentID)
+        {
+            OrderDetailRepo coRepo = new OrderDetailRepo(_context);
+            var query = coRepo.GetOrder(paymentID);
+            return View(query);
         }
     }
 }
---
+++ ThriftBook_phase2/Repositories/OrdersHistoryRepo.cs
@@ -0,0 +1,26 @@
+﻿using rolesDemoSSD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThriftBook_phase2.Data;
+
+namespace ThriftBook_phase2.Repositories
+{
+    public class OrdersHistoryRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public OrdersHistoryRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Invoice> GetOrdersLists(int buyerId)
+        {
+            var query = from i in _context.Invoice
+                        where i.BuyerId == buyerId
+                        select i;
+            return query;
+        }
+    }
+}

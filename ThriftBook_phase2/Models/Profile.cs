d68d5042fc36b5dea1f066524147c6ff1cfbf6e8
Ting Deng <dengting0122@gmail.com>
2022-04-11 16:54:36 -0700

    add regex for profile model

--- ThriftBook_phase2/Models/Profile.cs
+++ ThriftBook_phase2/Models/Profile.cs
@@ -28,15 +28,19 @@ namespace rolesDemoSSD.Models
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "This is not a valid last name.")]
         [StringLength(50, ErrorMessage = "Name must be maximum of 50 characters.")]
         public string LastName { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "This is not a valid city.")]
         public string City { get; set; }
+        [RegularExpression(@"^[A-Za-z0-9]+(?:\s[A-Za-z0-9'_-]+)+$", ErrorMessage = "This is not a valid street address.")]
         public string Street { get; set; }
 
-        [Display(Name = "Postal Code")]
+        [Display(Name = "Postal Code (eg. N1N 1N1)")]
+        [RegularExpression(@"[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]", ErrorMessage = "This is not a valid postal code.")]
         public string PostalCode { get; set; }
 
         [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
+        [RegularExpression(@"^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$", ErrorMessage = "This is not a valid phone number.")]
 
+        public string PhoneNumber { get; set; }
         public virtual ICollection<BookRating> BookRatings { get; set; }
         public virtual ICollection<Invoice> Invoices { get; set; }
     }
